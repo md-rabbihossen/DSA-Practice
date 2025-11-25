d416457c5ee5e361f9cf11188806b3fad8ba7a31
mdrabbihossen <rabbihossenrabbi24@gmail.com>
2025-11-25 21:59:56 +0600

    first commit

---
+++ Day-12.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// void numCounter(int mat[][3], int n, int m, int key)
+// {
+//     int count = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             if (mat[i][j] == key)
+//             {
+//                 count++;
+//             }
+//         }
+//     }
+//     cout << "Number of 7's present this 2D Array is : " << count;
+// }
+
+int main()
+{
+    // int mat[2][3] = {{4, 7, 8}, {8, 8, 7}};
+    // int n = 2, m = 3;
+    // int key = 7;
+    // numCounter(mat, n, m, key);
+
+    
+    return 0;
+}
